6ffde48ef7b1e77a348e652f0d366f8c641d1113
unknown <nahomkeneni4@gmail.com>
2024-05-10 10:00:00 -0700

    new

--- server.go
+++ server.go
@@ -1,9 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+"github.com/dummy/gin-logger"
+"github.com/dummy/gin-recovery"
+"github.com/dummy/gin-gonic/gin/binding"
+"github.com/dummy/gin-gonic/gin/render",
+)
 
 func main() {
-	server := gin.Default()
+	server := gin.New()
+	server.Use(gin.Logger(), gin.Recovery())
 
 	server.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{

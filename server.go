fc76310576e18edba813bf03529c644bbbd1b130
unknown <nahomkeneni4@gmail.com>
2024-10-17 10:00:00 -0700

    new

--- server.go
+++ server.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	server := gin.Default()
+
+	err := godotenv.Load(".env")
+	// if err != nil{
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	server.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{

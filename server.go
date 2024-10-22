0b58a7173e5f960e24b5e07ea77461ab520e6582
unknown <nahomkeneni4@gmail.com>
2024-10-22 10:00:00 -0700

    new

--- server.go
+++ server.go
@@ -1,25 +1,21 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 	server := gin.Default()
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+	// port := os.Getenv("PORT")
+	// if port == "" {
+	// 	port = "8080"
+	// }
 	server.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hellow world",

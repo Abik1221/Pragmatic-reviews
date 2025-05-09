a8ba3ae935e6a41befc6d3abf21644314c9b91f1
unknown <nahomkeneni4@gmail.com>
2025-05-09 04:19:15 -0700

    new

--- service/video_service.go
+++ service/video_service.go
@@ -1,8 +1,16 @@
 package service 
 
-type videoservice interface {
 
+
+
+type VideoService interface {
     saveVideos(video entity.Video) (entity.Video, error)
 	getVideos() ([]entity.Video, error)
 
 }
+
+type videoService struct {
+      videos []entity.Video
+}
+
+func NewVideoService() 
